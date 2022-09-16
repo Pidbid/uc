57d55bafdd26f24d6592a0d8a79a8a05297692cc
Wicos <wicos@wicos.cn>
2022-09-16 20:22:29 +0800

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# uc
+# UC
 一个简化uniCloud cli开发的工具
+## 应用场景
+在使用 uniCloud 开发时，需要调用Hbuilder进行云函数的上传，如果不想使用Hbuilder上传云函数，可以采用cli的方式，但cli方式命令行过长，且不易使用，本工具便简化其使用。
+
+## 如何使用
+下载Releases内最符合您操作系统的版本，将uc.exe 文件放置在您的运行目录下，首次运行会在当前目录下生成一个ucConfig.json文件，请使用记事本或者编辑器打开该文件，并将hbuilder运行目录填写在hbuilder对应的位置，object与provider不用填写，如：  
+`
+{
+    "hbuilder":"D:\HbuilderX",
+    "object":"",
+    "provider":""
+}
+`  
+填写之后，即可使用如下方法进行操作  
+1. **列举资源信息**  
+`uc.exe -l [resource]` 如 `uc.exe -l cf` 表示：列举当前项目中所有云端函数  
+2. **资源上传[默认覆盖非跳过]**  
+`uc.exe -u [resource] [name]` 如 `uc.exe -u cf test` 表示：上传名称为test的云函数，如果云端存在同名函数则覆盖  
+3. **资源下载[默认覆盖非跳过]**  
+`uc.exe -d [resource] [name]` 如 `uc.exe -d cf test` 表示：下载名称为test的云函数，如果本地存在同名函数则覆盖  
+4. resource的取值如下  
+|resource  | 含义 |  
+| ----------- | ----------- |  
+| cf 或 cloudfunction | 云函数 |  
+| cm 或 common | 云函数的公共模块 |  
+| db | 数据集合Schema |  
+| vf | 数据库校验函数  |  
+| ac | 数据库触发条件 |  
+| sp | 云空间 |  
+
+## 关于作者
+[歪克士的博客](https://wicos.me)
+
+
---
+++ uc.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+'''
+@File    :   cu.py
+@Time    :   2022/09/16 15:38:12
+@Author  :   Wicos 
+@Version :   1.0
+@Site    :   https://wicos.me
+@Email   :   wicos@wicos.cn
+'''
+
+import os
+import json
+import subprocess
+import getopt
+import sys
+
+
+class cloudUp:
+    def __init__(self) -> None:
+        self.path = os.path.realpath(__file__).replace("\\", "/")
+        self.__config_init__()
+
+    def __config_init__(self):
+        if not os.path.isfile("ucConfig.json"):
+            with open("./ucConfig.json", "w+") as fp:
+                fp.write(json.dumps(
+                    {"hbuilder": "", "provider": "", "project": ""}, indent=4))
+        json_file = open(os.path.split(self.path)[0]+"/ucConfig.json")
+        config = json.load(json_file)
+        if "\\" in config['hbuilder']:
+            config['hbuilder'] = config['hbuilder'].replace("\\", "/")
+            with open(os.path.split(self.path)[0]+"/ucConfig.json", "w+") as fp:
+                fp.write(json.dumps(config, indent=4))
+            json_file = open(os.path.split(self.path)[0]+"/ucConfig.json")
+            config = json.load(json_file)
+        self.cli = config['hbuilder']+'/cli.exe'
+        self.provider = config['provider'] or self.__provider_init__()
+        self.project = config['project'] or self.path.split("/")[-2]
+        if config["project"] == "" or config["provider"] == "":
+            config["project"] = self.project
+            config["provider"] = self.provider
+            with open(os.path.split(self.path)[0]+"/ucConfig.json", "w+") as fp:
+                fp.write(json.dumps(config, indent=4))
+
+    def __provider_init__(self):
+        file_list = os.listdir()
+        for i in file_list:
+            if "uniCloud-aliyun" in i:
+                return "aliyun"
+            elif "uniCloud-tcb" in i:
+                return "tcb"
+        return None
+
+    def __get_resource__(self, resource):
+        if resource in ["cf", "cloudfunction"]:
+            self.resource = "cloudfunction"
+        if resource in ["cm", "common"]:
+            self.resource = "common"
+        if resource in ["db"]:
+            self.resource = "db"
+        if resource in ["vf"]:
+            self.resource = "vf"
+        if resource in ["ac", "action"]:
+            self.resource = "action"
+        if resource in ["sp", "space"]:
+            self.resource = "space"
+
+    # def
+    def __run__(self, action):
+        if action == "list":
+            result = subprocess.Popen(
+                "{} cloud functions --list {} --prj {} --provider {} --cloud".format(self.cli, self.resource, self.project, self.provider), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        if action == "upload":
+            result = subprocess.Popen(
+                "{} cloud functions --upload {} --prj {} --provider {} --name {} --force".format(self.cli, self.resource, self.project, self.provider, self.name), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        if action == "download":
+            result = subprocess.Popen(
+                "{} cloud functions --download {} --prj {} --provider {} --name {} --force".format(self.cli, self.resource, self.project, self.provider, self.name), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while result.poll() is None:
+            print_out = result.stdout.readline().decode("utf-8")
+            if print_out != "":
+                print(print_out)
+
+    def start(self):
+        opts, args = getopt.getopt(sys.argv[1:], "uld", [
+                                   "upload", "list", "download"])
+        action = ""
+        for o, a in opts:
+            # action
+            if o in ["-u", "--upload"]:
+                action = "upload"
+            if o in ["-d", "--download"]:
+                action = "download"
+            if o in ["-l", "--list"]:
+                action = "list"
+        if action == "list":
+            if len(args) != 1:
+                print("-l 模式下只能输入一个资源模式 如 -l cf 或 -l cloudfunction")
+                return 0
+            else:
+                self.__get_resource__(args[0])
+                self.__run__(action)
+        if action == "upload":
+            if len(args) != 2:
+                print("-u 模式下必须输入两个值 如 -u cf fun1 表示上传fun1云函数至云空间  默认强制覆盖")
+                return 0
+            else:
+                self.__get_resource__(args[0])
+                self.name = args[1]
+                self.__run__(action)
+        if action == "download":
+            if len(args) != 2:
+                print("-d 模式下必须输入两个值 如 -d cf fun1 表示下载fun1云函数至本地  默认强制覆盖")
+                return 0
+            else:
+                self.__get_resource__(args[0])
+                self.name = args[1]
+                self.__run__(action)
+
+
+if __name__ == "__main__":
+    cu = cloudUp()
+    cu.start()
